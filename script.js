5aef1c1c843c67bc96f5115822a9090e169f2031
JhoneMello <jhonecmello@gmail.com>
2025-08-30 20:27:38 -0300

    Finalizando tarefa count

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const contador = document.getElementById('count');
 const decrementar = document.getElementById('decrement');
 const resertar = document.getElementById('reset');
 const incrementar = document.getElementById('increment');
+let fundo = document.getElementById("fundo");
+
 
 
 let numero = 0;
@@ -10,6 +12,29 @@ let numero = 0;
 contador.innerText = numero;
 
 decrementar.addEventListener('click', () => {
+  let cor = window.getComputedStyle(fundo).backgroundColor;
+  fundo.style.backgroundColor = "red";
   numero = numero - 1;
   contador.innerText = numero;
+
 });
+
+resertar.addEventListener('click', () => {
+  let cor = window.getComputedStyle(fundo).backgroundColor;
+  fundo.style.backgroundColor = "aqua";
+  numero = 0;
+  contador.innerText = numero;
+
+
+});
+
+
+incrementar.addEventListener('click', () => {
+  let cor = window.getComputedStyle(fundo).backgroundColor;
+  fundo.style.backgroundColor = "green";
+  numero = numero + 1;
+  contador.innerText = numero;
+
+});
+
+  
